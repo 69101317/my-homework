d45eef816644625f8570599560ea4f0cc8daf688
69101317 <2197215715@qq.com>
2026-07-19 23:37:33 +0800

    修正仓库名称为my-homework，完整博客配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 export default {
   title: "我的个人博客",
   description: "课程实验搭建VitePress静态博客",
-  base: "/blog-demo/",
+  base: "/my-homework/",
   themeConfig: {
     nav: [{ text: "首页", link: "/" }],
     sidebar: [{ text: "第一篇博客", link: "/posts/first-blog" }]
